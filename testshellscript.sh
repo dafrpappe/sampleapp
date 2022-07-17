c457b4b74285140cc465d0bda2d7536201899838
David Pappe <fpappe@hotmail.com>
2022-07-16 22:28:23 -0600

    test- ADD-13

--- testshellscript.sh
+++ testshellscript.sh
@@ -2,11 +2,11 @@
 echo "Add two number in shell."
 echo "From git"
 echo "system change 2022"
-echo "PASSED"
+echo "PASSED-2"
 
-sum=$(($num1 + $num2)))
+sum=$(($num1 + $num2))
 echo ${sum}
 echo "DONE"
 echo ${sum}
 echo ${sum}
-echo ${sum}
+
